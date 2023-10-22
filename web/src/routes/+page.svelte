dfeb10e004c68fbc0c69c4a070fcab9fec414fb7
Elijah Potter <elijah.sirius@protonmail.com>
2023-10-21 20:58:18 -0600

    Switched to better highlighting system

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,53 +1,31 @@
 <script lang="ts">
 	import Suggestion from '$lib/Suggestion.svelte';
 	import alice from '../../../alice.txt?raw';
-	import { position } from 'caret-pos';
 	import { highlightText } from '$lib/parse';
 
 	let content = alice;
-	let editor: HTMLDivElement;
+	let highlighted = '';
+	let highlight: HTMLDivElement;
+	let editor: HTMLTextAreaElement;
 
-	function updateContent() {
-		if (editor !== null) {
-			content = editor.innerText;
-		}
-	}
-
-	function highlight(node: HTMLDivElement, text: string) {
-		function action(text: string) {
-			if (editor) {
-				const pos = position(editor);
-				highlightText(text).then((highlighted) => {
-					node.innerHTML = highlighted;
-					position(editor, pos.pos);
-				});
-			} else {
-				highlightText(text).then((highlighted) => {
-					node.innerHTML = highlighted;
-				});
-			}
-		}
-		action(text);
-		return {
-			update(text: string) {
-				action(text);
-			},
-			onchange(text: string) {
-				action(text);
-			}
-		};
-	}
+	$: highlightText(content).then((v) => (highlighted = v));
 </script>
 
 <div class="flex flex-row w-full h-screen">
-	<div
-		class="flex-auto h-full p-5"
-		contenteditable="true"
-		use:highlight={content}
-		bind:this={editor}
-		placeholder="Tell your story..."
-		on:input={updateContent}
-	/>
+	<div class="flex-auto h-full p-5 grid z-10">
+		<div class="overflow-auto" style="grid-row: 1; grid-column: 1" bind:this={highlight}>
+			<div class="whitespace-pre-wrap break-words">
+				{@html highlighted}
+			</div>
+		</div>
+		<textarea
+			class="w-full h-full m-0 rounded-none p-0 z-0 bg-transparent border-none"
+			style="grid-row: 1; grid-column: 1"
+			bind:value={content}
+			on:scroll={() => highlight.scrollTo(editor.scrollTop)}
+			bind:this={editor}
+		></textarea>
+	</div>
 	<div class="flex flex-col flex-none">
 		<Suggestion category="grammar" title="Word Repeated">
 			<p>The word "the" was repeated twice</p>
