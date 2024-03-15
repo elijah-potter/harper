3056b27cdb8a17fe1d049be763460ce4463b7d6c
Elijah Potter <elijah.sirius@protonmail.com>
2024-03-15 11:39:22 -0600

    feat: added obsidian plugin

---
+++ packages/obsidian-plugin/rollup.config.mjs
@@ -0,0 +1,26 @@
+import { wasm } from '@rollup/plugin-wasm';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
+export default {
+	input: 'src/main.js',
+	output: {
+		dir: '.',
+		format: 'cjs'
+	},
+	external: [
+		"obsidian",
+		"electron",
+		"@codemirror/autocomplete",
+		"@codemirror/collab",
+		"@codemirror/commands",
+		"@codemirror/language",
+		"@codemirror/lint",
+		"@codemirror/search",
+		"@codemirror/state",
+		"@codemirror/view",
+		"@lezer/common",
+		"@lezer/highlight",
+		"@lezer/lr",
+	],
+  plugins: [wasm({ maxFileSize: Math.pow(2, 32), publicPath: "./" }), nodeResolve()]
+};
