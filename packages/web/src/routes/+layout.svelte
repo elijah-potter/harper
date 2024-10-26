41fb7fd5b68a3db33744fcbc860267f5b15e0c0d
Elijah Potter <me@elijahpotter.dev>
2024-10-26 09:40:39 -0600

    feat(web): swapped out font

--- packages/web/src/routes/+layout.svelte
+++ packages/web/src/routes/+layout.svelte
@@ -4,6 +4,10 @@
 	import { DarkMode } from 'flowbite-svelte';
 </script>
 
+<link
+	href="https://fonts.googleapis.com/css2?family=Atkinson+Hyperlegible:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+	rel="stylesheet"
+/>
 <div class="h-16" />
 <div class="absolute top-8 right-8"><DarkMode /></div>
 
